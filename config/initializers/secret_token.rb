8ef95d730bbbfd528e94e067bb8733d16776e575
Lorna Marlow <bikergirl1515@hotmail.com>
2015-11-17 21:40:35 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ChessApp::Application.config.secret_key_base = 'dbbfcc43aba2e4766455848aa2e4e509abbed7c7ef8825ead3f01015bf9e863d967b910e22fd47438ea8d2d1ddbd0a1fddc48ddd3474bc1652ca8dd00dad8eb8'
