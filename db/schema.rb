046de09a9c49d09f77390d75906defe974973eb8
Mark Eberhard <mce777@gmail.com>
2015-11-20 09:39:15 +0000

    pieces model migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151119230208) do
+ActiveRecord::Schema.define(version: 20151119232103) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,14 @@ ActiveRecord::Schema.define(version: 20151119230208) do
     t.integer  "white_player_id"
     t.integer  "black_player_id"
     t.integer  "winner_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "pieces", force: true do |t|
+    t.string   "type"
+    t.string   "color"
+    t.integer  "game_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
