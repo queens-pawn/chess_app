218d0378869d5c475e07605fcefae25ee489a060
Alan <alanlim25@gmail.com>
2015-11-27 16:00:27 +0000

    Changed y position for king and queen

--- app/models/game.rb
+++ app/models/game.rb
@@ -14,8 +14,8 @@ class Game < ActiveRecord::Base
     Piece.create {:type => 'Bishop', :color => 'white', :game_id => id, :x_position => 0, :y_position => 2}
     Piece.create {:type => 'Bishop', :color => 'white', :game_id => id, :x_position => 0, :y_position => 5}
 
-    Piece.create {:type => 'King', :color => 'white', :game_id => id, :x_position => 0, :y_position => 3}
-    Piece.create {:type => 'Queen', :color => 'white', :game_id => id, :x_position => 0, :y_position => 4}
+    Piece.create {:type => 'King', :color => 'white', :game_id => id, :x_position => 0, :y_position => 4}
+    Piece.create {:type => 'Queen', :color => 'white', :game_id => id, :x_position => 0, :y_position => 3}
 
     (0..7).each do |y|
       Piece.create {:type => 'Pawn', :color => 'black', :game_id => id, :x_position => 6, :y_position => y}
