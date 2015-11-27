c2ca8d8a9c8934ffa446a8a79f53b0bafa728fe3
Alan <alanlim25@gmail.com>
2015-11-27 15:40:54 +0000

    Created white Knight pieces

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,9 +2,13 @@ class Game < ActiveRecord::Base
   after_create :populate_board!
 
   def populate_board!
-    (0..7).each do |x|
-      Piece.create {:type => 'Pawn', :color => 'white', :game_id => id, :x_position => x, :y_position => 1}
+    (0..7).each do |y|
+      Piece.create {:type => 'Pawn', :color => 'white', :game_id => id, :x_position => 1, :y_position => y}
     end
     Piece.create {:type => 'Rook', :color => 'white', :game_id => id, :x_position => 0, :y_position => 0}
+    Piece.create {:type => 'Rook', :color => 'white', :game_id => id, :x_position => 0, :y_position => 7}
+
+    Piece.create {:type => 'Knight', :color => 'white', :game_id => id, :x_position => 0, :y_position => 1}
+    Piece.create {:type => 'Knight', :color => 'white', :game_id => id, :x_position => 0, :y_position => 6}
   end
 end
