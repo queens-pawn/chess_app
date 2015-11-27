124abf6ce250f179aca748a0df52c2605db5d4cd
Alan <alanlim25@gmail.com>
2015-11-27 15:36:42 +0000

    Created white pawn pieces

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,9 @@
 class Game < ActiveRecord::Base
+  after_create :populate_board!
+
+  def populate_board!
+    (0..7).each do |x|
+      Piece.create {:type => 'Pawn', :color => 'white', :game_id => id, :x_position => x, :y_position => 1}
+    end
+  end
 end
