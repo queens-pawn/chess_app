f2fad25072db9757862a85ea29f949f80dda8bc3
Alan <alanlim25@gmail.com>
2015-11-27 15:37:11 +0000

    Created white rook pieces

--- app/models/game.rb
+++ app/models/game.rb
@@ -5,5 +5,6 @@ class Game < ActiveRecord::Base
     (0..7).each do |x|
       Piece.create {:type => 'Pawn', :color => 'white', :game_id => id, :x_position => x, :y_position => 1}
     end
+    Piece.create {:type => 'Rook', :color => 'white', :game_id => id, :x_position => 0, :y_position => 0}
   end
 end
