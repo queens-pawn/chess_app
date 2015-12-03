709a4ceb4102e295306dd976cd9c20489d7b0417
Alan <alanlim25@gmail.com>
2015-12-03 03:46:26 +0000

    Testing on factories.rb

--- test/models/game_test.rb
+++ test/models/game_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class GameTest < ActiveSupport::TestCase
 
   test "populate board" do
-    g = FactoryGirl.create(:game)
-    assert_equal 32, g.pieces.count
+    game = FactoryGirl.create(:game)
+    assert_equal 32, game.pieces.count
   end
 end
