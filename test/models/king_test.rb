c29d1f1c13b4a8530ea84d82ddd94acb9019f9c9
JohannBillar <jwbillar@gmail.com>
2015-12-06 00:31:09 +0000

    Create method valid_move? for King piece.

--- test/models/king_test.rb
+++ test/models/king_test.rb
@@ -1,7 +1,59 @@
 require 'test_helper'
 
 class KingTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def test_valid_move?
+    king = King.new(x_position: 4, y_position: 4)
+
+    assert(
+      king.valid_move?(5, 4)
+     ) 
+    assert(
+      king.valid_move?(4, 5)
+     )
+      assert(
+      king.valid_move?(3, 4)
+     ) 
+      assert(
+      king.valid_move?(4, 3)
+     ) 
+      assert(
+      king.valid_move?(5, 5)
+     ) 
+      assert(
+      king.valid_move?(3, 5)
+     ) 
+      assert(
+      king.valid_move?(3, 3)
+     ) 
+      assert(
+      king.valid_move?(5, 3)
+     ) 
+  end
+
+  def test_not_valid_move?
+    king = King.new(x_position: 4, y_position: 0)
+
+    assert ! (
+      king.valid_move?(4, -1)
+     ) 
+    assert ! (
+      king.valid_move?(3, 3)
+     )
+    assert ! (
+      king.valid_move?(5, 3)
+     ) 
+    assert ! (
+      king.valid_move?(4, 2)
+     )  
+    assert ! (
+      king.valid_move?(6, 0)
+     )
+    assert ! (
+      king.valid_move?(2, 0)
+     )
+    assert ! (
+      king.valid_move?(6, 0)
+     )         
+  end
+  
 end
