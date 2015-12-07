3c3b0a9be8240aec8eac7a451465303f5ea73156
Alan <alanlim25@gmail.com>
2015-12-07 20:31:49 +0000

    Test push origin

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -1,6 +1,5 @@
 class PiecesController < ApplicationController
 
-<<<<<<< HEAD
   # def show
   #   @game = current_piece.game
   #   @pieces = current_piece.game.pieces
@@ -9,14 +8,6 @@ class PiecesController < ApplicationController
   def update
     @game = current_piece.game
 
-=======
-  def show
-    @game = current_piece.game
-    @pieces = current_piece.game.pieces
-  end
-
-  def update
->>>>>>> e7068d99febd159db0a25f47bbfd9e720d1122dc
     x = params[:x_position]
     y = params[:y_position]
     current_piece.update_attributes(:x_position => x, :y_position => y)
