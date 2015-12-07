8cd073d9ecc9728bb77c792b193d58349139c5f7
Mark Eberhard <mce777@gmail.com>
2015-12-07 19:45:21 +0000

    git

--- app/models/king.rb
+++ app/models/king.rb
@@ -1,11 +1,5 @@
 class King < Piece
 
-<<<<<<< HEAD
-  def to_my_image_path
-    # in assets/images
-    "king_#{color}.svg"
-
-=======
   def possible_moves(x_position, y_position)
     x, y = x_position, y_position
     width, height = 8, 8
@@ -25,8 +19,12 @@ class King < Piece
 
   def valid_move?(new_x_position, new_y_position)
     new_position = [new_x_position, new_y_position]
-    possible_moves(x_position, y_position).include? new_position 
->>>>>>> master
+    possible_moves(x_position, y_position).include? new_position
+  end
+
+  def to_my_image_path
+    # in assets/images
+    "king_#{color}.svg"
   end
 
 end
