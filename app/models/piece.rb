b5eed0051904d42c36728a8e767175ebdb048a0a
Mark Eberhard <mce777@gmail.com>
2015-12-09 22:57:46 +0000

    move_to method 1

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -9,4 +9,20 @@ class Piece < ActiveRecord::Base
   def validate_color?
     errors.add(:color, 'Invalid color') unless color == 'white' || color == 'black'
   end
+
+  def move_to!(new_x, new_y)
+
+    if piece exists at new_x, new_y
+      if current_piece.color == other_piece.color
+        return an error message -- not valid move
+      else
+        other_piece => goes offboard
+      end
+      current_piece.update(x_position: new_x, y_position: new_y)
+    else
+      current_piece.update(x_position: new_x, y_position: new_y)
+    end
+
+  end
+
 end
