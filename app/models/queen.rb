0cd029897d9de8eceb7f85382ddd36e87bd8e03c
Alan <alanlim25@gmail.com>
2015-12-10 03:58:06 +0000

    Attempting queen logic

--- app/models/queen.rb
+++ app/models/queen.rb
@@ -1,9 +1,13 @@
 class Queen < Piece
 
+  def valid_move?(x_position, y_positison)
+    x, y = x_position, y_position
+    x_position == x || y_position == y || (x_position - x).abs == (y_position - y).abs
+  end
+
   def to_my_image_path
     # in assets/images
     "queen_#{color}.svg"
-
   end
 
 end
--- test/models/queen_test.rb
+++ test/models/queen_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class QueenTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "Queen valid move" do
+    queen = Queen.create(x_position: 3, y_position: 7)
+    assert_equal true, queen.valid_move?(0, 7)
+    assert_equal true, queen.valid_move?(3, 2)
+  end
 end
