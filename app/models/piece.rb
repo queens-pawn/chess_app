2174006cb2a33ec60578f05647c249589e1421de
Lorna Marlow <bikergirl1515@hotmail.com>
2015-12-11 10:06:41 +0000

    is_obstructed? method

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -9,4 +9,59 @@ class Piece < ActiveRecord::Base
   def validate_color?
     errors.add(:color, 'Invalid color') unless color == 'white' || color == 'black'
   end
+
+  def is_vertical?(x_destination, y_destination)
+  	self.x_position == x_destination
+  end
+
+  def is_horizontal?(x_destination, y_destination)
+  	self.y_position == y_destination
+  end
+
+  def is_diagonal?(x_destination, y_destination)
+  	x_destination - x_position == y_destination - y_position
+  end
+
+  def vertical_obstruction?(x_destination, y_destination)
+  	is_obstructed = false
+  	((self.y_position + 1)..y_destination).each do |y|
+	  	# get a game piece with same game_id as current piece at (x, y) coordinate
+	  	piece = Piece.where("game_id = ? AND x_position = ? AND y_position = ?" , game_id , x_destination, y)
+	  	#return true if there is a piece and false if there isn't
+	  	is_obstructed = piece.any?
+	  	break if is_obstructed 
+		end
+		is_obstructed
+  end
+
+  def horizontal_obstruction?(x_destination, y_destination)
+  	is_obstructed = false
+  	((self.x_position + 1)..x_destination).each do |x|
+  		piece = Piece.where("game_id = ? AND x_position = ? AND y_position = ?", game_id, x, y_destination)
+  		is_obstructed = piece.any?
+  		break if is_obstructed
+  	end
+  	is_obstructed
+  end
+
+  def diagonal_obstruction?(x_destination, y_destination)
+  	is_obstructed = false
+  	y_pos = (self.y_position + 1)
+  	((self.x_position + 1)..x_destination).each do |x|
+  		piece = Piece.where("game_id = ? AND x_position = ? AND y_position = ?", game_id, x, y_pos)
+  		is_obstructed = piece.any?
+  		break if is_obstructed
+  		y_pos += 1
+  	end
+  	is_obstructed
+  end
+
+	def is_obstructed?(x_destination, y_destination)
+		return diagonal_obstruction?(x_destination, y_destination) if is_diagonal?(x_destination, y_destination)
+    return vertical_obstruction?(x_destination, y_destination) if is_vertical?(x_destination, y_destination)
+    return horizontal_obstruction?(x_destination, y_destination) if is_horizontal?(x_destination, y_destination)
+    raise 'Invalid input. Not diagonal, horizontal or vertical.'
+  end
 end
+
+ 
--- test/models/piece_test.rb
+++ test/models/piece_test.rb
@@ -1,7 +1,127 @@
 require 'test_helper'
 
 class PieceTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	test "is_vertical_true" do 
+    piece = Piece.create(:type => 'King', :color => 'white', :game_id => 1, :x_position => 3, :y_position => 4)
+    expected = true
+    actual = piece.is_vertical?(3, 7)
+    assert_equal expected, actual
+  end
+
+  test "is_vertical_false" do 
+    piece = Piece.create(:type => 'King', :color => 'white', :game_id => 1, :x_position => 3, :y_position => 4)
+    expected = false
+    actual = piece.is_vertical?(5, 7)
+    assert_equal expected, actual
+  end
+
+  test "is_horizontal_true" do 
+    piece = Piece.create(:type => 'King', :color => 'white', :game_id => 1, :x_position => 3, :y_position => 4)
+    expected = true
+    actual = piece.is_horizontal?(7, 4)
+    assert_equal expected, actual
+  end
+
+  test "is_horizontal_false" do 
+    piece = Piece.create(:type => 'King', :color => 'white', :game_id => 1, :x_position => 3, :y_position => 4)
+    expected = false
+    actual = piece.is_horizontal?(7, 5)
+    assert_equal expected, actual
+  end
+
+  test "is_diagonal_true" do 
+    piece = Piece.create(:type => 'King', :color => 'white', :game_id => 1, :x_position => 3, :y_position => 4)
+    expected = true
+    actual = piece.is_diagonal?(6, 7)
+    assert_equal expected, actual
+  end
+
+  test "is_diagonal_false" do 
+    piece = Piece.create(:type => 'King', :color => 'white', :game_id => 1, :x_position => 3, :y_position => 4)
+    expected = false
+    actual = piece.is_diagonal?(4, 6)
+    assert_equal expected, actual
+  end
+
+  test "vertical_obstruction_true?" do
+  	piece = Piece.create(:type => 'King', :color => 'white', :game_id => 1, :x_position => 3, :y_position => 4)
+  	pawn = Piece.create(:type => 'Pawn', :color => 'white', :game_id => 1, :x_position => 3, :y_position => 5)
+  	expected = true
+    actual = piece.vertical_obstruction?(3, 6)
+    assert_equal expected, actual
+  end
+
+  test "vertical_obstruction_false?" do
+  	piece = Piece.create(:type => 'King', :color => 'white', :game_id => 1, :x_position => 6, :y_position => 2)
+  	pawn = Piece.create(:type => 'Pawn', :color => 'white', :game_id => 1, :x_position => 6, :y_position => 6)
+  	expected = false
+    actual = piece.vertical_obstruction?(6, 4)
+    assert_equal expected, actual
+  end
+
+  test "horizontal_obstruction_true?" do
+  	piece = Piece.create(:type => 'King', :color => 'white', :game_id => 1, :x_position => 3, :y_position => 4)
+  	pawn = Piece.create(:type => 'Pawn', :color => 'white', :game_id => 1, :x_position => 5, :y_position => 4)
+  	expected = true
+    actual = piece.horizontal_obstruction?(7, 4)
+    assert_equal expected, actual
+  end
+
+  test "horizontal_obstruction_false?" do
+  	piece = Piece.create(:type => 'King', :color => 'white', :game_id => 1, :x_position => 3, :y_position => 4)
+  	pawn = Piece.create(:type => 'Pawn', :color => 'white', :game_id => 1, :x_position => 7, :y_position => 4)
+  	expected = false
+    actual = piece.horizontal_obstruction?(5, 4)
+    assert_equal expected, actual
+  end
+
+  test "diagonal_obstruction_true?" do
+  	piece = Piece.create(:type => 'King', :color => 'white', :game_id => 1, :x_position => 2, :y_position => 2)
+  	pawn = Piece.create(:type => 'Pawn', :color => 'white', :game_id => 1, :x_position => 4, :y_position => 4)
+  	expected = true
+    actual = piece.diagonal_obstruction?(5, 5)
+    assert_equal expected, actual
+  end
+
+  test "diagonal_obstruction_false?" do
+  	piece = Piece.create(:type => 'King', :color => 'white', :game_id => 1, :x_position => 4, :y_position => 1)
+  	pawn = Piece.create(:type => 'Pawn', :color => 'white', :game_id => 1, :x_position => 8, :y_position => 5)
+  	expected = false
+    actual = piece.diagonal_obstruction?(6, 3)
+    assert_equal expected, actual
+  end
+
+  test "is_obstructed_horizontal?" do
+  	piece = Piece.create(:type => 'King', :color => 'white', :game_id => 1, :x_position => 4, :y_position => 4)
+  	pawn = Piece.create(:type => 'Pawn', :color => 'white', :game_id => 1, :x_position => 5, :y_position => 4)
+  	expected = true
+  	actual = piece.is_obstructed?(8, 4)
+  	assert_equal expected, actual
+  end
+
+  test "is_obstructed_vertical?" do
+  	piece = Piece.create(:type => 'King', :color => 'white', :game_id => 1, :x_position => 5, :y_position => 4)
+  	pawn = Piece.create(:type => 'Pawn', :color => 'white', :game_id => 1, :x_position => 5, :y_position => 7)
+  	expected = true
+  	actual = piece.is_obstructed?(5, 8)
+  	assert_equal expected, actual
+  end
+
+  test "is_obstructed_diagonal?" do
+  	piece = Piece.create(:type => 'King', :color => 'white', :game_id => 1, :x_position => 2, :y_position => 4)
+  	pawn = Piece.create(:type => 'Pawn', :color => 'white', :game_id => 1, :x_position => 5, :y_position => 7)
+  	expected = true
+  	actual = piece.is_obstructed?(6, 8)
+  	assert_equal expected, actual
+  end
+
+  test "is_obstructed_invalid?" do
+  	piece = Piece.create(:type => 'King', :color => 'white', :game_id => 1, :x_position => 4, :y_position => 4)
+  	pawn = Piece.create(:type => 'Pawn', :color => 'white', :game_id => 1, :x_position => 5, :y_position => 4)
+  	expected = true
+  	actual = piece.is_obstructed?(2, 5)
+  	#assert_equal("Invalid input. Not diagonal, horizontal or vertical.", exception.message)
+  	#assert_equal expected, actual
+  	assert_raise(RuntimeError) { raise("Invalid input. Not diagonal, horizontal or vertical.") }
+  end
 end
