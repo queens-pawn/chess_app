0ec6782223dab274694a1d9f55ea14cc59e91b75
Mark Eberhard <mce777@gmail.com>
2015-12-11 20:48:26 +0000

    capture logic 3

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -10,21 +10,25 @@ class Piece < ActiveRecord::Base
     errors.add(:color, 'Invalid color') unless color == 'white' || color == 'black'
   end
 
-  def move_to!(new_x, new_y)
-    move_to_position = [x_position: new_x, y_position: new_y]
+  def move_to!(x_position: nil, y_position: nil)
+    another_piece = self.game.piece_at(x_position, y_position)
 
-    # if the move_to_position is NOT empty...
-    if !move_to_position.empty?
-      if current_piece.color == other_piece.color
-        return an error message -- not valid move
-      else
-        other_piece => goes offboard
+    if another_piece #exists...
+      if another_piece.color != self.color #check if same color
+        another_piece.destroy
+        successful_move!(x_position, y_position)
       end
-      current_piece.update(x_position: new_x, y_position: new_y)
     else
-      current_piece.update(x_position: new_x, y_position: new_y)
+      successful_move!(x_position, y_position)
     end
 
+  end
+
+  private
+
+  def successful_move!(x_position: nil, y_position: nil)
+    self.update(x_position: x_position, y_position: y_position)
+    :success
   end
 
 end
