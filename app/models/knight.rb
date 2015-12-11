0857ff69faecbd271a66ed222a6da6894f7889ae
Lorna Marlow <bikergirl1515@hotmail.com>
2015-12-11 14:01:39 +0000

    completed valid move for knight

--- app/models/knight.rb
+++ app/models/knight.rb
@@ -1,9 +1,28 @@
 class Knight < Piece
 
+	def valid_move?(x_destination, y_destination)
+		#check whether out of bounds
+		return false if out_of_bounds?(x_destination, y_destination)
+		#make sure the length of move is correct
+		correct_length?(x_destination, y_destination)
+	end
+
+	#similar to pull request to be added app/models/piece.rb to be moved out once added, maybe renamed?
+	def out_of_bounds?(x_destination, y_destination)
+		if x_destination < 0 || x_destination > 7
+			return true
+		elsif y_destination < 0 || y_destination > 7
+			return true
+		end
+	end
+
+	#check to see if move is of correct length
+	def correct_length?(x_destination, y_destination)
+		(x_position - x_destination) + (y_position - y_destination) == 3
+	end
+
   def to_my_image_path
     # in assets/images
     "knight_#{color}.svg"
-
   end
-
 end
--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 20151125152305) do
 
   create_table "games", force: :cascade do |t|
     t.integer  "game_id"
-    t.string   "name"
+    t.string   "name",            limit: 255
     t.integer  "white_player_id"
     t.integer  "black_player_id"
     t.integer  "winner_id"
@@ -27,8 +27,8 @@ ActiveRecord::Schema.define(version: 20151125152305) do
   end
 
   create_table "pieces", force: :cascade do |t|
-    t.string   "type"
-    t.string   "color"
+    t.string   "type",       limit: 255
+    t.string   "color",      limit: 255
     t.integer  "game_id"
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -37,18 +37,18 @@ ActiveRecord::Schema.define(version: 20151125152305) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+    t.string   "email",                  limit: 255, default: "", null: false
+    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",                      default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                                      null: false
+    t.datetime "updated_at",                                      null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
