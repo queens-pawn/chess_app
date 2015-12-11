99a9225bee21ddbe6cc3e75e5a42671037e222f1
alan24 <alanlim25@gmail.com>
2015-12-11 11:29:00 -0600

    Update Piece.rb
    Added def valid_move? to attempt to pass the test on continuous integration.

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -9,4 +9,13 @@ class Piece < ActiveRecord::Base
   def validate_color?
     errors.add(:color, 'Invalid color') unless color == 'white' || color == 'black'
   end
+  
+  def valid_move?(x, y)
+    if x < 0 || x > 7
+      return false
+    elsif y < 0 || y > 7
+      return false
+    end
+    return true
+  end
 end
