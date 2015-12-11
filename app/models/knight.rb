4f23efeb1307968b6e4aef3ee7d82f09a946b29d
alan24 <alanlim25@gmail.com>
2015-12-11 10:29:28 -0600

    knight.rb
    Refactored the code by removing def out_of_bounds? 
    Refactored the code by removing def correct_length? and put inside the def valid_move?.
    Used super for inheritance for the piece.rb.
    I also changed the argument variable from x_destination and y_destination to x and y. (Not sure if this change is needed

--- app/models/knight.rb
+++ app/models/knight.rb
@@ -1,25 +1,17 @@
 class Knight < Piece
   #make sure doesnt include is_obstructed method when added to piece as can jump over pieces.
-	def valid_move?(x_destination, y_destination)
+	def valid_move?(x, y)
 		#check whether out of bounds
-		return false if out_of_bounds?(x_destination, y_destination)
+		unless super(x, y)
+			return false
+		end
 		#make sure the length of move is correct
-		correct_length?(x_destination, y_destination)
+		(x_position - x).abs + (y_position - y).abs == 3
 	end
 
 	#similar to pull request to be added app/models/piece.rb to be moved out once added, maybe renamed?
-	def out_of_bounds?(x_destination, y_destination)
-		if x_destination < 0 || x_destination > 7
-			return true
-		elsif y_destination < 0 || y_destination > 7
-			return true
-		end
-	end
 
 	#check to see if move is of correct length
-	def correct_length?(x_destination, y_destination)
-		(x_position - x_destination).abs + (y_position - y_destination).abs == 3
-	end
 
   def to_my_image_path
     # in assets/images
