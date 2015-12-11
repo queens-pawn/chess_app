9e6856a3db59807e1fe26bedc48a69efdabdc5a5
JohannBillar <jwbillar@gmail.com>
2015-12-11 17:07:51 +0000

    Refactored King Valid_move?

--- app/models/king.rb
+++ app/models/king.rb
@@ -1,25 +1,10 @@
 class King < Piece
 
-  def possible_moves(x_position, y_position)
-    x, y = x_position, y_position
-    width, height = 8, 8
-
-    moves = [
-      [(x+1), y], [x, (y+1)],
-      [(x-1), y], [x, (y-1)],
-      [(x+1), (y+1)], [(x-1), (y+1)],
-      [(x-1), (y-1)], [(x+1), (y-1)]
-    ]
-
-    moves.select do |coord|
-      x, y = coord[0], coord[1]
-      x >= 0 && (x <= (width - 1)) && y >= 0 && (y <= (height - 1))
+  def valid_move?(x, y)
+    unless super(x, y)
+      return false
     end
-  end
-
-  def valid_move?(new_x_position, new_y_position)
-    new_position = [new_x_position, new_y_position]
-    possible_moves(x_position, y_position).include? new_position
+    (x_position - x).abs <= 1 && (y_position - y).abs <= 1 
   end
 
   def to_my_image_path
