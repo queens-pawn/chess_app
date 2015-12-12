5685b40d397bb52ccbe177a392e2518a6806f9cc
Mark Eberhard <mce777@gmail.com>
2015-12-12 21:16:58 +0000

    deleted unused code

--- test/controllers/pieces_controller_test.rb
+++ test/controllers/pieces_controller_test.rb
@@ -1,11 +1,10 @@
 require 'test_helper'
 
 class PiecesControllerTest < ActionController::TestCase
-  
+
   test "should get update" do
     piece = FactoryGirl.create(:piece)
     put :update, :id => piece.id, :piece => { :x_position => 1, :y_position => 2 }
-    # put :update, :id => piece.id, piece.move_to! => { :x_position => 1, :y_position => 2}
     piece.reload
     assert_equal 1, piece.x_position
     assert_equal 2, piece.y_position
