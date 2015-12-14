94d90c328e295679b2042a852943656fa0e5b6c7
Lorna Marlow <bikergirl1515@hotmail.com>
2015-12-14 13:23:53 +0000

    Added test for valid diagonal backwards move

--- test/models/bishop_test.rb
+++ test/models/bishop_test.rb
@@ -2,11 +2,15 @@ require 'test_helper'
 
 class BishopTest < ActiveSupport::TestCase
 	def setup
-    @bishop = Bishop.create(x_position: 0, y_position: 0)
+    @bishop = Bishop.create(x_position: 2, y_position: 2)
   end
 
   test "Bishop valid diagonal move" do
     assert_equal true, @bishop.valid_move?(3, 3)
+  end
+
+  test "Bishop valid diagonal backwards" do
+    assert_equal true, @bishop.valid_move?(1, 1)
   end
 
   test "Bishop diagonal invalid move" do
