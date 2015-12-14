fe66c023eed7f970b31d029f5990a4463f2c5a97
Lorna Marlow <bikergirl1515@hotmail.com>
2015-12-14 13:00:32 +0000

    completed valid move for bishop

--- test/models/bishop_test.rb
+++ test/models/bishop_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class BishopTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	def setup
+    @bishop = Bishop.create(x_position: 0, y_position: 0)
+  end
+
+  test "Bishop valid diagonal move" do
+    assert_equal true, @bishop.valid_move?(3, 3)
+  end
+
+  test "Bishop diagonal invalid move" do
+    assert_equal false, @bishop.valid_move?(3, 2)
+  end
+
+  test "Bishop position off board" do
+    assert_equal false, @bishop.valid_move?(-1, -1)
+  end
 end
