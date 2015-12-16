a54ff6d57b0883bc2b8b036ce755ddd2f7372409
Alan <alanlim25@gmail.com>
2015-12-16 16:24:31 +0000

    Added pawn to move foward

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -1,11 +1,26 @@
 class Pawn < Piece
 
+  def valid_move?(x, y)
+    unless super(x, y)
+      return false
+    end
+    if color == 'white'
+      if y_position == 6
+        (x_position - x == 0 && y_position - y == 1) || (x_position - x == 0 && y_position - y == 2)
+      else
+        x_position - x == 0 && y_position - y == 1
+      end
+    else
+      if y_position == 1
+        (x - x_position == 0 && y - y_position == 1) || (x - x_position == 0 && y - y_position == 2)
+      else
+        x - x_position == 0 && y - y_position == 1
+      end
+    end
+  end
+
   def to_my_image_path
     # in assets/images
     "pawn_#{color}.svg"
-
   end
-
-
-
 end
--- test/models/pawn_test.rb
+++ test/models/pawn_test.rb
@@ -1,7 +1,51 @@
 require 'test_helper'
 
 class PawnTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @first_white_pawn = Pawn.create(color: 'white', x_position: 3, y_position: 6)
+    @second_white_pawn = Pawn.create(color: 'white', x_position: 3, y_position: 3)
+    @first_black_pawn = Pawn.create(color: 'black', x_position: 0, y_position: 1)
+    @second_black_pawn = Pawn.create(color: 'black', x_position: 0, y_position: 2)
+  end
+
+  test "First move white 1 space foward" do
+    assert_equal true, @first_white_pawn.valid_move?(3, 5)
+  end
+
+  test "White first move pawn 2 space foward" do
+    assert_equal true, @first_white_pawn.valid_move?(3, 4)
+  end
+
+  test "Invalid too many steps foward(first move)" do
+    assert_equal false, @first_white_pawn.valid_move?(3, 3)
+    assert_equal false, @first_black_pawn.valid_move?(0, 5)
+  end
+
+  test "Invalid backward move" do
+    assert_equal false, @first_white_pawn.valid_move?(3, 7)
+    assert_equal false, @second_white_pawn.valid_move?(3, 4)
+    assert_equal false, @first_black_pawn.valid_move?(0, 0)
+    assert_equal false, @second_black_pawn.valid_move?(0, 1)
+  end
+
+  test "After white first move, 1 space foward" do
+    assert_equal true, @second_white_pawn.valid_move?(3, 2)
+  end
+
+  test "Invalid move, moved too many steps foward" do
+    assert_equal false, @second_white_pawn.valid_move?(3, 1)
+    assert_equal false, @second_black_pawn.valid_move?(0, 4)
+  end
+
+  test "First move black 1 space foward" do
+    assert_equal true, @first_black_pawn.valid_move?(0, 2)
+  end
+
+  test "First move black 2 space foward" do
+    assert_equal true, @first_black_pawn.valid_move?(0, 3)
+  end
+
+  test "After black first move, 1 space foward" do
+    assert_equal true, @second_black_pawn.valid_move?(0, 3)
+  end
 end
