a6e0455d826acec64c2eed21cc515b23aa823522
Mark Eberhard <mce777@gmail.com>
2015-12-18 19:13:15 +0000

    controller test issues

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -2,7 +2,7 @@ class PiecesController < ApplicationController
 
   def update
     @game = current_piece.game
-    if current_piece.valid_move?
+    if current_piece.valid_move?(x_position: piece_params[:x_position], y_position: piece_params[:y_position])
       current_piece.move_to!(x_position: piece_params[:x_position], y_position: piece_params[:y_position])
       redirect_to game_path(@game)
     end
