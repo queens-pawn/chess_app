a06f0c69f3a8738e5d2c9e2b46a5667aad5b72a0
Mark Eberhard <mce777@gmail.com>
2015-12-18 22:04:45 +0000

    added :type to pieces controller test

--- test/controllers/pieces_controller_test.rb
+++ test/controllers/pieces_controller_test.rb
@@ -4,7 +4,7 @@ class PiecesControllerTest < ActionController::TestCase
 
   test "should get update" do
     piece = FactoryGirl.create(:piece)
-    put :update, :id => piece.id, :piece => { :x_position => 1, :y_position => 2 }
+    put :update, :id => piece.id, :type => 'Pawn', :piece => { :x_position => 1, :y_position => 2 }
     piece.reload
     assert_equal 1, piece.x_position
     assert_equal 2, piece.y_position
