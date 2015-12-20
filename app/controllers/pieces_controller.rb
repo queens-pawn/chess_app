24f796c08d199e2f008715a8a839d56b96450150
JohannBillar <jwbillar@gmail.com>
2015-12-20 21:21:39 +0000

    submit x and y coords using ajax put

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -3,7 +3,8 @@ class PiecesController < ApplicationController
   def update
     @game = current_piece.game
     current_piece.move_to!(x_position: piece_params[:x_position], y_position: piece_params[:y_position])
-    redirect_to game_path(@game)
+    # redirect_to game_path(@game)
+    render :text => 'updated!'
   end
 
   private
